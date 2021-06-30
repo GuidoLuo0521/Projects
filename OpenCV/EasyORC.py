20f39b48794919e06f6630844ef7d3bbf69f93ba
Guido <15823159025@163.com>
2021-06-30 23:51:28 +0800

    绘制出最大轮廓

--- OpenCV/EasyORC.py
+++ OpenCV/EasyORC.py
@@ -1,6 +1,7 @@
 # 一个 简单 ORC
 
 import cv2
+import numpy as np
 
 def ORC_Imread(path):
     return cv2.imread(path)    
@@ -20,37 +21,43 @@ def ORC_Canny(mat):
 def ORC_Close(mat):
     return cv2.morphologyEx(mat, cv2.MORPH_CLOSE, cv2.getStructuringElement(  cv2.MORPH_RECT, (11, 11)))
     
+def ORC_GetArea(mat):
+    return cv2.contourArea(mat)
+    
 # 轮廓检测 绘制最外框的轮廓
-def ORC_FindContours(raw, mat):
+def ORC_FindMaxAreaContours(raw, mat):
+
+    # contours 标记的轮廓，以 list 形式存在，每个轮廓中都包含了轮廓像素的坐标向量；
+    # hierarchy 表示轮廓的继承关系，一般用不到;d
     contours, hierarchy = cv2.findContours(mat.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
 
-    # 遍历轮廓
-    for c in cnts:
-        # 计算轮廓近似
-        peri = cv2.arcLength(c, True)
-        # c表示输入的点集，epsilon表示从原始轮廓到近似轮廓的最大距离，它是一个准确度参数
-        approx = cv2.approxPolyDP(c, 0.02*peri, True)
- 
-        # 4个点的时候就拿出来
-        if len(approx) == 4:
-            screenCnt = approx
-            break
+    # 获取最大的一个轮廓，当然这里主意这里可能没检测到
+    cnt = sorted(contours, key=ORC_GetArea, reverse=True)[0]
 
-    return cv2.drawContours(raw, [screenCnt], -1, (0, 255, 0), 2)
+    # 外接图形
+    box = cv2.minAreaRect(cnt)
+    # 获取点矩形所有点
+    rect_points = cv2.boxPoints(box)
+    #利用 np 填充所有点
+    rect_points = np.int0(rect_points)
 
+    # 绘制最小外接矩形
+    return cv2.drawContours(raw, [rect_points], 0, (0, 0, 255), 2)
+
+    # [cnt] 将参数转为数组
+    #return cv2.drawContours(raw, [box], -1, (0, 255, 0), 2)
 
 
 if __name__ == "__main__" :
 
     # 读取图片
-    mat = ORC_Imread("images/image-20210629230839952.png")
+    raw = ORC_Imread("images/image-20210629230839952.png")
 
     # 重置大小
-    mat = ORC_Resize(mat)
+    raw = ORC_Resize(raw)
 
     # 高斯滤波
-    mat = ORC_GaussianBlur(mat)
+    mat = ORC_GaussianBlur(raw)
 
     # 变灰度图
     mat = ORC_ToGrayColor(mat)
@@ -62,7 +69,8 @@ if __name__ == "__main__" :
     mat = ORC_Close(mat)
     cv2.imshow("pre process", mat)
 
-    mat = ORC_FindContours(ORC_Imread("images/image-20210629230839952.png"), mat)
+    
+    mat = ORC_FindMaxAreaContours( raw, mat)
 
     cv2.imshow("comtours", mat)
     cv2.waitKey(0)
