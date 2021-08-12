13dcabf3c3ed1af73728faebc223e3399bcf6e33
Guido <15823159025@163.com>
2021-08-12 23:20:51 +0800

    数据库插入值

--- CMS/InsertDateToDB/main.cpp
+++ CMS/InsertDateToDB/main.cpp
@@ -10,10 +10,6 @@
 
 #include "RandomParams.h"
 
-
-
-
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -23,12 +19,11 @@ int main(int argc, char *argv[])
     guidocms.setPort(3306);
     guidocms.setDatabaseName("guidocms");
     guidocms.setUserName("root");
-    guidocms.setPassword("millet");
-
-
+    guidocms.setPassword("4474ljx");
 
     QStringList strListName;
-    QFile file("D:/github/Projects/CMS/DevRecords/MySQL/name.txt");
+    QString strFilePath = "D:/github/Projects/CMS/DevRecords/MySQL/name.txt";
+    QFile file(strFilePath);
     if( file.open(QIODevice::ReadOnly | QIODevice::Text) )
     {
         QTextStream stream(&file);
@@ -41,8 +36,7 @@ int main(int argc, char *argv[])
         file.close();
     }
 
-    bool isV  = guidocms.isValid();
-    //if(guidocms.open())
+    if(guidocms.open())
     {
         const int knCount = strListName.size();
         for( int i = 0; i < knCount; ++i)
@@ -53,9 +47,10 @@ int main(int argc, char *argv[])
             QString Birthday, Department, JobName;
             float Wage = 0.0;
             int WorkingAge = 0;
-            QString Place, Education, Specialty, Address="00000000000", EMail="00000000000", Phone="00000000000", EnterCampany, LeaveCampany="0000-00-00", Introduction="暂无";
+            QString Place, Education, Specialty, Address="", EMail="", Phone="",
+                    EnterCampany, LeaveCampany="0000-00-00", Introduction="";
             int Resignation = 0;
-            QString Param = "暂无";
+            QString Param = "";
 
             StaffID = 0;
             StaffName = strListName[i];
@@ -81,24 +76,20 @@ int main(int argc, char *argv[])
             Resignation = GetRandomResignation();
 
             QString strSql = QString(" INSERT INTO staff "
-                                     "VALUES ( %1, %2, %3, %4, %5, "
-                                     "%6, %7, %8, %9, %10, "
-                                     "%11, %12, %13, %14, %15, "
-                                     "%16, %17, %18, %19, %20, %21);")
+                                     "VALUES (%1,'%2','%3',%4,'%5',"
+                                     "'%6','%7',%8,%9,'%10',"
+                                     "'%11','%12','%13','%14','%15',"
+                                     "'%16','%17','%18',%19,'%20');")
                     .arg(StaffID).arg(StaffName).arg(StaffPassword).arg(Sex).arg(Birthday)
                     .arg(Department).arg(JobName).arg(Wage).arg(WorkingAge).arg(Place)
-                    .arg(Place).arg(Education).arg(Specialty).arg(Address).arg(EMail)
-                    .arg(Phone).arg(EnterCampany).arg(LeaveCampany).arg(Introduction).arg(Resignation)
-                    .arg(Param);
+                    .arg(Education).arg(Specialty).arg(Address).arg(EMail).arg(Phone)
+                    .arg(EnterCampany).arg(LeaveCampany).arg(Introduction).arg(Resignation).arg(Param);
 
-            qDebug() << strSql;
-
-            //guidocms.exec(strSql);
-            //qDebug() << guidocms.lastError().text();
+            guidocms.exec(strSql);
         }
 
     }
-    qDebug() << guidocms.lastError().text();
+    qDebug() << "exec end.";
 
     return a.exec();
 }
